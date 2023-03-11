2c2aacbc3c017ad5be534e5a56e32b389e612537
yurukute <nguyenkhanhdung.9999@gmail.com>
2023-03-11 14:41:33 +0700

    reformat code

--- esp8266_car.ino
+++ esp8266_car.ino
@@ -2,87 +2,98 @@
 #include <BlynkSimpleEsp8266.h>
 
 //FIRMWARE CONFIGURATION
+#define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPLwxz7Etgp"
 #define BLYNK_TEMPLATE_NAME "Quickstart Template"
 #define BLYNK_AUTH_TOKEN "s3_jVWTCNlIVRDlTlMGAvxWOXxsJ84PT"
 
-// Variables for the Blynk widget values
+// Define motor pins
+#define IN1 D1
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+
+// Joystick's default values
 int x = 50;
 int y = 50;
 
-// Get the joystick values
-BLYNK_WRITE(V0) {
-  x = param[0].asInt();
-}
-// Get the joystick values
-BLYNK_WRITE(V1) {
-  y = param[0].asInt();
-}
-
-// Define the motor pins
-const int motors[5] = {0, D1, D2, D3, D4};
-
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   //Set the motor pins as output pins
-  for(int i = 1; i <= 4; i++)
+  int motors[] = {IN1, IN2, IN3, IN4};
+  for(int i = 0; i < 4; i++)
     pinMode(motors[i], OUTPUT);
   // Initialize the Blynk library
   char ssid[] = ""; //Enter your WIFI name
-  char pass[] = ""; //Enter your WIFI passowrd
+  char pass[] = ""; //Enter your WIFI password
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
 }
 
-// Check these values using the IF condition
-void smartCar() {
+void loop() {
+  Blynk.run(); // Run the blynk function
   if (y > 70) {
     carForward();
     Serial.println("carForward");
-  } else if (y < 30) {
+
+  } 
+  else if (y < 30) {
     carBackward();
     Serial.println("carBackward");
-  } else if (x < 30) {
+  } 
+  else if (x < 30) {
     carLeft();
     Serial.println("carLeft");
-  } else if (x > 70) {
+  } 
+  else if (x > 70) {
     carRight();
     Serial.println("carRight");
-  } else if (x < 70 && x > 30 && y < 70 && y > 30) {
+  } 
+  else if (x < 70 && x > 30 && y < 70 && y > 30) {
     carStop();
     Serial.println("carStop");
   }
 }
 
-void loop() {
-  Blynk.run();// Run the blynk function
-  smartCar();// Call the main function
+//Get Blynk values
+BLYNK_WRITE(V0) {
+  x = param[0].asInt();
 }
 
-/**************Motor movement functions*****************/
-void carForward() {
-  for(int i = 1; i <= 4; i++){
-    digitalWrite(motors[i], (i % 2) ? HIGH : LOW);
-  }
+BLYNK_WRITE(V1) {
+  y = param[0].asInt();
 }
+
+//Motor movement functions
+void carForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
 void carBackward() {
-  for(int i = 1; i <= 4; i++){
-    digitalWrite(motors[i], (i % 2) ? LOW : HIGH);
-  }
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
 void carLeft() {
-  for(int i = 1; i <= 4; i++){
-    digitalWrite(motors[i], (i > 1 && i < 4) ? LOW : HIGH);
-  }
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 void carRight() {
-  for(int i = 1; i <= 4; i++){
-    digitalWrite(motors[i], (i > 1 && i < 4) ? HIGH : LOW);
-  }
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
 void carStop() {
-  for(int i = 1; i <= 4; i++)
-    digitalWrite(motors[0], LOW);
+  int motors[] = {IN1, IN2, IN3, IN4};
+  for(int i = 0; i < 4; i++)
+    digitalWrite(motors[i], LOW);
 }
